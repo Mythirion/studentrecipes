4aea8ade4225fed23ae2167d0fe0475c331c0ce1
Aaron Daisley <daisleya@coventry.ac.uk>
2017-12-01 16:57:10 +0000

    Modified to point at my local port.

--- server.js
+++ server.js
@@ -12,25 +12,12 @@ var recipes = require("./routes/recipes");
 app.use(bodyparser.urlencoded({
   extended: true
 }));
-//app.use(cors({origin: 'http://localhost'}));
+app.use(cors({origin: 'http://localhost:4200'}));
 //app.use(function(req,res,next) {
 //  res.header("Access-Control-Allow-Origin", "*");
 //  res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept");
 //  next();
 //});
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
 
 // User routing
 app.post('/api/users', users.add);
